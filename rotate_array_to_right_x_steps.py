4a27b0386cc418f6b7fba389cd39e3f8add31f4b
Adesh Valecha <adesh.valecha@gmail.com>
2019-02-07 21:58:08 -0700

    Create rotate_array_to_right_x_steps.py

---
+++ rotate_array_to_right_x_steps.py
@@ -0,0 +1,8 @@
+# Given an array, rotate the array to the right by k steps, where k is non-negative.
+class Solution:
+    def rotate(self, nums: 'List[int]', k: 'int') -> 'None':
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            nums.insert(0,nums.pop())
