2c194d1d54b64a000524528255a1f58bd54c9a1d
Adesh Valecha <adesh.valecha@gmail.com>
2019-02-08 22:20:55 -0700

    Create incides_two_nos_matching_sum.py

---
+++ incides_two_nos_matching_sum.py
@@ -0,0 +1,12 @@
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+class Solution:
+    def twoSum(self, nums: 'List[int]', target: 'int') -> 'List[int]':
+        
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+       
