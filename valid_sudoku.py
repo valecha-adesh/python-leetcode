beb377a92af8c4258f761a94442e198223214ac7
Adesh Valecha <adesh.valecha@gmail.com>
2019-02-08 21:47:17 -0700

    Update valid_sudoku.py

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -5,41 +5,39 @@
 #Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
 
 class Solution:
-
-#    def c_list(self,l):
-#        for x in range(9):
-#            if l.count(str(x)) > 1:
-#                return False
-#            else:
-#                return True
-
-                
-    
     def isValidSudoku(self, board: 'List[List[str]]') -> 'bool':
         
-        for i in range(9):
+        def c_list(self,l:'List[str]'):
             for x in range(1,10):
-                if board[i].count(str(x)) > 1:
+                if l.count(str(x)) > 1:
                     return False
+            return True
+        
+        for i in range(9):
+            res = c_list(self,board[i])
+            if res == False:
+                return False
         
         for i in range(9):
             temp = []
             for j in range(9):
                 temp.append(board[j][i])  
-            for x in range(1,10):
-                if temp.count(str(x)) > 1:
-                    return False
+            
+            res = c_list(self,temp)
+            if res == False:
+                return False
+            
         
         
         temp_00 = []
         for i in range(3):
             for j in range(3):
                     temp_00.append(board[i][j])
-        
-        for x in range(1,10):
-            if temp_00.count(str(x)) > 1:
-                return False
-            
+                    
+        res = c_list(self,temp_00)
+        if res == False:
+            return False
+          
                     
         
         temp_10 = []
@@ -47,79 +45,74 @@ class Solution:
             for j in range(3):
                     temp_10.append(board[i][j])
         
-        for x in range(1,10):
-            if temp_10.count(str(x)) > 1:
-                return False
-            
-            
+                    
+        res = c_list(self,temp_10)
+        if res == False:
+            return False
+                    
         temp_20 = []
         for i in range(6,9):
             for j in range(3):
                     temp_20.append(board[i][j])
-        
-        for x in range(1,10):
-            if temp_20.count(str(x)) > 1:
-                return False
-            
+                  
+        res = c_list(self,temp_20)
+        if res == False:
+            return False
             
             
         temp_01 = []
         for i in range(3):
             for j in range(3,6):
                     temp_01.append(board[i][j])
-        
-        for x in range(1,10):
-            if temp_01.count(str(x)) > 1:
-                return False
             
+        res = c_list(self,temp_01)
+        if res == False:
+            return False
             
         temp_11 = []
         for i in range(3,6):
             for j in range(3,6):
                     temp_11.append(board[i][j])
-        
-        for x in range(1,10):
-            if temp_11.count(str(x)) > 1:
-                return False
             
+        res = c_list(self,temp_11)
+        if res == False:
+            return False
             
         temp_21 = []
         for i in range(6,9):
             for j in range(3,6):
                     temp_21.append(board[i][j])
-        
-        for x in range(1,10):
-            if temp_21.count(str(x)) > 1:
-                return False
-            
+
+        res = c_list(self,temp_21)
+        if res == False:
+            return False
             
         temp_02 = []
         for i in range(3):
             for j in range(6,9):
                     temp_02.append(board[i][j])
-        
-        for x in range(1,10):
-            if temp_02.count(str(x)) > 1:
-                return False
             
+        res = c_list(self,temp_02)
+        if res == False:
+            return False
             
         temp_12 = []
         for i in range(3,6):
             for j in range(6,9):
                     temp_12.append(board[i][j])
-        
-        for x in range(1,10):
-            if temp_12.count(str(x)) > 1:
-                return False
             
+        res = c_list(self,temp_12)
+        if res == False:
+            return False
             
         temp_22 = []
         for i in range(6,9):
             for j in range(6,9):
                     temp_22.append(board[i][j])
+            
+        res = c_list(self,temp_22)
+        if res == False:
+            return False
         
-        for x in range(1,10):
-            if temp_22.count(str(x)) > 1:
-                return False
             
         return True
